fb7a7c55f4af40848ca7c2c82adff8203d5acc76
max <frismo98@gmail.com>
2020-12-08 09:11:41 -0500

    Updated converters to match the new Column definitions.

--- src/converters.rs
+++ src/converters.rs
@@ -3,20 +3,27 @@ use crate::column::{Type, Value, Column};
 use regex::Regex;
 use chrono::{DateTime, NaiveDateTime, NaiveDate};
 
-trait Converter {
-    fn convert(&mut self, field: &str) -> Option<column::Value>;
-    fn column_type(&self) ->  column::Type;
+fn result_to_option<T, E>(result: Result<T, E>) -> Option<T> {
+    match result {
+        Ok(v) => Some(v),
+        Err(_) => None
+    }
+}
+
+pub trait Converter<T> {
+    fn convert(&mut self, field: &str) -> Option<T>;
+    fn make_column(values: Vec<T>) ->  column::Column;
 }
 
 pub struct ToString {}
 
-impl Converter for ToString {
-    fn convert(& mut self, field: &str) -> Option<column::Value> {
-        return Some(column::Value::String(field.to_owned()))
+impl Converter<String> for ToString {
+    fn convert(& mut self, field: &str) -> Option<String> {
+        Some(field.to_owned())
     }
 
-    fn column_type(&self) -> Type {
-        return Type::String;
+    fn make_column(values: Vec<String>) -> Column {
+        column::Column::Strings(values)
     }
 }
 
@@ -26,28 +33,21 @@ pub struct ToBool {}
 
 macro_rules! make_default_converter {
     ($name: tt, $type: tt, $enum: tt) => {
-        impl Converter for $name {
-            fn convert(&mut self, field: &str) -> Option<Value> {
-                match field.parse::<$type>() {
-                    Err(e) => {
-                        println!("{}", e);
-
-                        None
-                    },
-                    Ok(i) => Some(Value::$enum(i))
-                }
+        impl Converter<$type> for $name {
+            fn convert(&mut self, field: &str) -> Option<$type> {
+                result_to_option(field.parse::<$type>())
             }
 
-            fn column_type(&self) -> Type {
-                return column::Type::$enum;
+            fn make_column(values: Vec<i64>) -> Column {
+                column::Column::$enum(values)
             }
         }
     }
 }
 
-make_default_converter!(ToInt, i64, Int);
-make_default_converter!(ToFloat, f64, Float);
-make_default_converter!(ToBool, bool, Boolean);
+make_default_converter!(ToInt, i64, Ints);
+make_default_converter!(ToFloat, f64, Floats);
+make_default_converter!(ToBool, bool, Booleans);
 
 pub struct ToDate {
     valid_format: String,
@@ -63,22 +63,20 @@ impl ToDate {
     }
 }
 
-impl Converter for ToDate {
-    fn convert(&mut self, field: &str) -> Option<Value> {
+impl Converter<column::DateTime> for ToDate {
+    fn convert(&mut self, field: &str) -> Option<column::DateTime> {
         if self.valid_format_found {
-            match NaiveDate::parse_from_str(field, self.valid_format.as_str()) {
-                Err(_) => None,
-                Ok(d) => Some(Value::Date(d.and_hms(0, 0, 0).timestamp()))
-            }
+            result_to_option(NaiveDate::parse_from_str(field, self.valid_format
+                .as_str()).map(|v| {v.and_hms(0, 0, 0).timestamp()}))
         } else {
             match DateTime::parse_from_rfc2822(field) {
                 Err(_) => (),
-                Ok(d) => return Some(Value::Date(d.timestamp())),
+                Ok(d) => return Some(d.timestamp()),
             };
 
             match DateTime::parse_from_rfc3339(field) {
                 Err(_) => (),
-                Ok(d) => return Some(Value::Date(d.timestamp())),
+                Ok(d) => return Some(d.timestamp()),
             }
 
             // we should only hit this part once
@@ -91,7 +89,7 @@ impl Converter for ToDate {
                         self.valid_format = format.to_owned();
                         self.valid_format_found = true;
 
-                        return Some(Value::Date(d.and_hms(0, 0, 0).timestamp()));
+                        return Some(d.and_hms(0, 0, 0).timestamp());
                     }
                 }
             }
@@ -100,8 +98,8 @@ impl Converter for ToDate {
         }
     }
 
-    fn column_type(&self) -> Type {
-        return column::Type::Date;
+    fn make_column(values: Vec<i64>) -> Column {
+        column::Column::Dates(values)
     }
 }
 
@@ -119,14 +117,13 @@ mod test {
 
         match int_converter.convert(source) {
             None => assert!(false),
-            Some(Value::Int(i)) => assert_eq!(i, 12),
+            Some(i) => assert_eq!(i, 12),
             _ => assert!(false),
         }
 
         match float_converter.convert(source) {
             None => assert!(false),
-            Some(Value::Float(f)) => assert_eq!(f, 12.0),
-            _ => assert!(false),
+            Some(f) => assert_eq!(f, 12.0),
         }
     }
 
@@ -139,14 +136,12 @@ mod test {
 
         match int_converter.convert(source) {
             None => assert!(true),
-            Some(Value::Int(i)) => assert!(false),
-            _ => assert!(false),
+            Some(i) => assert!(false),
         }
 
         match float_converter.convert(source) {
             None => assert!(false),
-            Some(Value::Float(f)) => assert_eq!(f, 12.56),
-            _ => assert!(false),
+            Some(f) => assert_eq!(f, 12.56),
         }
     }
 
@@ -162,32 +157,27 @@ mod test {
 
         match c.convert(true_source) {
             None => assert!(false),
-            Some(Value::Boolean(b)) => assert!(b),
-            _ => assert!(false),
+            Some(b) => assert!(b),
         }
 
         match c.convert(false_source) {
             None => assert!(false),
-            Some(Value::Boolean(b)) => assert!(!b),
-            _ => assert!(false),
+            Some(b) => assert!(!b),
         }
 
         match c.convert(true_as_int) {
             None => assert!(true),
-            Some(Value::Boolean(b)) => assert!(false),
-            _ => assert!(false),
+            Some(b) => assert!(false),
         }
 
         match c.convert(false_as_int) {
             None => assert!(true),
-            Some(Value::Boolean(b)) => assert!(false),
-            _ => assert!(false),
+            Some(b) => assert!(false),
         }
 
         match c.convert(false_as_non_one) {
             None => assert!(true),
-            Some(Value::Boolean(b)) => assert!(false),
-            _ => assert!(false),
+            Some(b) => assert!(false),
         }
 
     }
@@ -199,7 +189,7 @@ mod test {
         let mut converter = ToDate::new();
 
         match converter.convert(date) {
-            Some(Value::Date(d)) => assert!(true),
+            Some(d) => assert!(true),
             _ => assert!(false),
         }
     }
