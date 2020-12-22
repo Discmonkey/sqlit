33cbe61e9ca96c6aebbd546525762928305ae1bb
max <mgrinchenko@progeny.net>
2020-12-21 21:32:51 -0500

    adjusted grammar since we also need to capture literals in expression

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,2 @@
 pub mod rdp;
+pub mod rdp_display;
---
+++ src/parser/rdp_display.rs
@@ -0,0 +1,9 @@
+use super::rdp::ParserNode;
+use std::fmt::{Formatter, Display};
+
+
+impl Display for ParserNode {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+
+    }
+}
